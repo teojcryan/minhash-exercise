26beb4fdfe2b88b7b237e479c85f72ac11745f32
teojcryan <u2128816@live.warwick.ac.uk>
2022-02-15 18:33:42 +0000

    update codes

--- code/functions.R
+++ code/functions.R
@@ -1,21 +1,30 @@
-kmer <- function(genome, k, toList = F){
+kmer <- function(genome_string, k, toList = F){
   hash <- new.env(hash = TRUE)
   
-  for (i in 1:(nchar(genome)-k+1)){
+  for (i in 1:(nchar(genome_string)-k+1)){
     # extract relevant kmer
     kmer <- substr(genome, i, i+k-1)
     
     # if kmer does not exist in hash, add key with new value = 1 (first count)
     if (!(kmer %in% ls(hash))) {
       hash[[kmer]] <- 1
-    } else {
+    } #else {
       # if kmer-key exists in hash, add 1 to value
-      hash[[kmer]] <- hash[[kmer]] + 1
-    }
+      #hash[[kmer]] <- hash[[kmer]] + 1
+    #}
     
     if (i %% 1E5 == 0) {paste('iteration', i)}
   }
   if (toList == T){as.list(hash)} else {hash}
+}
+
+kmer2 <- function(genome_string, k){
+  kmers <- seqR::count_kmers(genome_string, k,
+                               with_kmer_counts=TRUE,
+                               with_kmer_names=TRUE)
+  kmers <- colnames(kmers) # extract column names
+  kmers <- gsub('[[:punct:][:digit:]]', '', kmers) # make kmers human readable
+  return(kmers)
 }
 
 J_dist <- function(A, B){
--- code/main.R
+++ code/main.R
@@ -1,22 +1,28 @@
 library('seqinr') # Biological Sequences Retrieval and Analysis
+library('seqR') # Fast and Comprehensive K-Mer Counting Package
 library('digest') # Compact Hash Digests of R Objects
-library('kmer')
+#library('kmer')
 source('code/functions.R')
 
-r6 <- read.fasta(file = 'data/R6.fa')
+### Read data -------------
+R6 <- read.fasta(file = 'data/R6.fa')
+R6_genome = paste(toupper(R6[[1]]), collapse='') # extract genome as string
 
-genome = r6
+TIGR4 <- read.fasta(file = 'data/TIGR4.fa')
+TIGR4_genome = paste(toupper(TIGR4[[1]]), collapse='') # extract genome as string
 
 ### Reading k-mers --------------
 k = 14
-genome_string = paste(toupper(genome[[1]]), collapse='')
-r6_kmer <- kmer(genome_string, k) # takes too long
 
-# Try methods from existing packages
-# r6_kmer <- seqinr.count(genome, k) # Error: memory exhausted (limit reached?)
-# r6_kmer <- kmer.kcount(genome, k) # Error: cannot allocate vector of size 14.0 Gb
+R6_kmer <- kmer2(R6_genome, k)
+TIGR4_kmer <- kmer2(TIGR4_genome, k)
 
-### Simple Jaccard distances --------------===
+# Attempts using own function/ other packages
+# r6_kmer <- kmer(genome_string, k) # own function takes too long
+# r6_kmer <- seqinr::count(genome, k) # Error: memory exhausted (limit reached?)
+# r6_kmer <- kmer::kcount(genome, k) # Error: cannot allocate vector of size 14.0 Gb
+
+### Simple Jaccard distances --------------
 A = c('TTGAAAGAAAAACA','TGAAAGAAAAACAA','GAAAGAAAAACAAT',
       'AAAGAAAAACAATT','AAGAAAAACAATTT','AGAAAAACAATTTT','GAAAAACAATTTTG')
 B = c('TTGAAAGAAAAACA','TGAAAGAAAAACAA','GAAAGAAAAACAAT',
@@ -26,13 +32,14 @@ J_dist(A,B)
 
 ### MinHash Jaccard distances --------------
 # objective: take an unbiased sample of the k-mers and calculate Jaccard distance from this smaller subset
-s = 5 # sketch size
+s = 10000 # sketch size
 
-hash_A = unlist(lapply(A, digest, algo = "murmur32", serialize = F, seed = 0))
-sketch_A = sort(hash_A)[1:s]
-hash_B = unlist(lapply(B, digest, algo = "murmur32", serialize = F, seed = 0))
-sketch_B = sort(hash_B)[1:s]
+R6_hash = unlist(lapply(R6_kmer, digest, algo = "murmur32", serialize = F, seed = 0))
+R6_sketch = sort(R6_hash, method="quick")[1:s]
+cat(R6_sketch,file="output/R6_sketch.txt",sep="\n")
 
-sketch_A
-sketch_B
-J_dist(sketch_A, sketch_B)
+TIGR4_hash = unlist(lapply(TIGR4_kmer, digest, algo = "murmur32", serialize = F, seed = 0))
+TIGR4_sketch = sort(TIGR4_hash, method="quick")[1:s]
+cat(TIGR4_sketch,file="output/TIGR4_sketch",sep="\n")
+
+J_dist(R6_hash, TIGR4_hash)
